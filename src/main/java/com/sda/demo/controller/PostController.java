b34dbfeb244a7e450a988cc92ec6fa6b39c478ed
Justyna <justynalis9@gmail.com>
2019-06-30 15:55:54 +0200

    service added

--- src/main/java/com/sda/demo/controller/PostController.java
+++ src/main/java/com/sda/demo/controller/PostController.java
@@ -8,10 +8,10 @@ import com.sda.demo.service.PostService;
 import com.sda.demo.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -26,10 +26,31 @@ public class PostController {
 
     @Autowired
 
-    @GetMapping("/addpost")
+  /*  @GetMapping("/addpost")
     public ModelAndView addPost() {
+        Post post = new Post();
+
         return new ModelAndView("post", "newPost",
-                new PostDto());
+                    new PostDto());
+    }
+*/
+    @GetMapping("/addpost")
+    public String addPost(@RequestParam String text) {
+
+        Post post = new Post();
+        post.setText(text);
+        postRepository.save(post);
+        return "Saved";
+    }
+
+    @PostMapping("/allPosts")
+    public ModelAndView create() {
+        ModelAndView modelAndView = new ModelAndView();
+        Post post = new Post();
+        modelAndView.addObject(post);
+        modelAndView.setViewName("posts/create");
+        return modelAndView;
+
     }
 
     @RequestMapping("/listOfPosts/")
@@ -37,11 +58,15 @@ public class PostController {
         List<Post> posts = this.postService.findAll();
         if (posts.isEmpty()) {
             return "redirect";
-        }  else {
+        } else {
             posts = postService.findAll();
             return "listOfPosts";
-
         }
+    }
 
+    @ModelAttribute("currentUserName")
+    public String getCurrentUserName() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
     }
 }
+
--- src/main/java/com/sda/demo/service/PostService.java
+++ src/main/java/com/sda/demo/service/PostService.java
@@ -1,136 +1,26 @@
 package com.sda.demo.service;
 
+
 import com.sda.demo.entity.Post;
-import com.sda.demo.repository.PostRepository;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
-public class PostService implements PostRepository {
+public class PostService  {
+
+  public  List<Post> findAll(){
 
 
-    @Override
-    public List<Post> findAll() {
-        return null;
+      return findAll();
     }
-
-    @Override
-    public List<Post> findAll(Sort sort) {
-        return null;
-    }
-
-    @Override
-    public Page<Post> findAll(Pageable pageable) {
-        return null;
-    }
-
-    @Override
-    public List<Post> findAll(Iterable<Long> iterable) {
-        return null;
-    }
-
-    @Override
-    public long count() {
-        return 0;
-    }
-
-    @Override
-    public void delete(Long aLong) {
+    /*
+  public  Post create(Post post){
 
     }
-
-    @Override
-    public void delete(Post post) {
+   public Post edit(Post post){
 
     }
-
-    @Override
-    public void delete(Iterable<? extends Post> iterable) {
+ public  void deleteById(Long id){
 
     }
-
-    @Override
-    public void deleteAll() {
-
-    }
-
-    @Override
-    public <S extends Post> S save(S s) {
-        return null;
-    }
-
-    @Override
-    public <S extends Post> List<S> save(Iterable<S> iterable) {
-        return null;
-    }
-
-    @Override
-    public Post findOne(Long aLong) {
-        return null;
-    }
-
-    @Override
-    public boolean exists(Long aLong) {
-        return false;
-    }
-
-    @Override
-    public void flush() {
-
-    }
-
-    @Override
-    public <S extends Post> S saveAndFlush(S s) {
-        return null;
-    }
-
-    @Override
-    public void deleteInBatch(Iterable<Post> iterable) {
-
-    }
-
-    @Override
-    public void deleteAllInBatch() {
-
-    }
-
-    @Override
-    public Post getOne(Long aLong) {
-        return null;
-    }
-
-    @Override
-    public <S extends Post> S findOne(Example<S> example) {
-        return null;
-    }
-
-    @Override
-    public <S extends Post> List<S> findAll(Example<S> example) {
-        return null;
-    }
-
-    @Override
-    public <S extends Post> List<S> findAll(Example<S> example, Sort sort) {
-        return null;
-    }
-
-    @Override
-    public <S extends Post> Page<S> findAll(Example<S> example, Pageable pageable) {
-        return null;
-    }
-
-    @Override
-    public <S extends Post> long count(Example<S> example) {
-        return 0;
-    }
-
-    @Override
-    public <S extends Post> boolean exists(Example<S> example) {
-        return false;
-    }
-
-
+*/
 }
